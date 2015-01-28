4aa3e3f82c6af6a3da0c19e46270c3718cb243e5
Brian Ge <bge@salesforce.com>
2015-01-28 01:18:26 -0800

    when we can't reach oauth server, and user's access token has already been revoked, bubble up the error for delegate to handle

--- shared/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFAuthenticationManager.m
+++ shared/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFAuthenticationManager.m
@@ -1228,6 +1228,9 @@ static Class InstanceClass = nil;
                                                          if (authInfo.authType == SFOAuthTypeUserAgent) {
                                                              [weakSelf log:SFLogLevelError msg:@"Network failure for OAuth User Agent flow is a fatal error."];
                                                              return NO;  // Default error handler will show the error.
+                                                         } else if ([SFUserAccountManager sharedInstance].currentUser.credentials.accessToken == nil) {
+                                                             [weakSelf log:SFLogLevelInfo msg:@"Network failure for OAuth Refresh flow (existing credentials)  Access token has been revoked."];
+                                                             return NO;
                                                          } else {
                                                              [weakSelf log:SFLogLevelInfo msg:@"Network failure for OAuth Refresh flow (existing credentials)  Try to continue."];
                                                              [weakSelf loggedIn:YES];
