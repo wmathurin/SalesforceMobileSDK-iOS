48d236e0cfc302b809c12e49ab7cd464be0eb0c6
j-neves <j.neves@salesforce.com>
2015-07-06 22:56:21 -0700

    Adding CSFNetwork shared instance manager

---
+++ libs/SalesforceNetwork/SalesforceNetwork/SalesforceNetwork/Classes/Network/Queue/CSFNetworkSharedInstancesManager.h
@@ -0,0 +1,13 @@
+//
+//  CSFNetworkSharedInstancesManager.h
+//  SalesforceNetwork
+//
+//  Created by João Neves on 7/6/15.
+//  Copyright (c) 2015 salesforce.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSFNetworkSharedInstancesManager : NSObject
+
+@end
---
+++ libs/SalesforceNetwork/SalesforceNetwork/SalesforceNetwork/Classes/Network/Queue/CSFNetworkSharedInstancesManager.m
@@ -0,0 +1,13 @@
+//
+//  CSFNetworkSharedInstancesManager.m
+//  SalesforceNetwork
+//
+//  Created by João Neves on 7/6/15.
+//  Copyright (c) 2015 salesforce.com. All rights reserved.
+//
+
+#import "CSFNetworkSharedInstancesManager.h"
+
+@implementation CSFNetworkSharedInstancesManager
+
+@end
