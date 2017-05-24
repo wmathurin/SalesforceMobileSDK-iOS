411132143d623052cf6f8253ca06f3c193a0f111
Arnold Goldberg <agoldberg@salesforce.com>
2017-05-24 10:21:19 -0500

    Fixed compile error

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Network/Network/Model/CSFOutput.m
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Network/Network/Model/CSFOutput.m
@@ -93,6 +93,7 @@ static NSString * const kCSFInputCustomArrayAttributes = @"__CSFOutput_Array_Sto
             
             Ivar ivar = class_getInstanceVariable(ivarInfo[@"class"], [ivarName UTF8String]);
             Class ivarClass = CSFClassFromEncoding(ivarInfo[@"encoding"]);
+            NSString * encoding = ivarInfo[@"encoding"];
             if (ivarClass) {
                 id result = [decoder decodeObjectOfClass:ivarClass forKey:propertyName];
                 if ([result isKindOfClass:[CSFOutput class]]) {
