b5f8d23f9c2320b2b650d308feef28fbcb9f0cc4
Aqeel Syed <aqeel.syed@salesforce.com>
2018-05-21 10:55:00 -0700

    Cherry pick fix for password policy check for multiple users. #2525

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFSecurityLockout.h
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFSecurityLockout.h
@@ -128,6 +128,7 @@ typedef void (^SFPasscodeViewControllerPresentationBlock)(UIViewController*);
 @end
 
 @class SFOAuthCredentials;
+@class SFUserAccount;
 
 /**
  This class interacts with the inactivity timer.
@@ -174,6 +175,13 @@ typedef void (^SFPasscodeViewControllerPresentationBlock)(UIViewController*);
  be subject to that policy.
  */
 + (void)clearPasscodeState;
+
+/**
+ Resets the passcode state of the app, *if* there aren't other users with an overriding passcode
+ policy.  I.e. passcode state can only be cleared if the  user is the only user who would
+ be subject to that policy.
+ */
++ (void)clearPasscodeState:(SFUserAccount *)userLoggingOut;
 
 /** Initialize the timer
  */
