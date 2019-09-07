9157e10d946d56aba46bf6db3a54d93999eb4323
Wolfgang Mathurin <wmathurin@salesforce.com>
2019-09-07 13:27:07 -0700

    Doing strong/weak dance

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Common/SalesforceSDKManager.m
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Common/SalesforceSDKManager.m
@@ -989,11 +989,13 @@ static NSInteger const kDefaultCacheDiskCapacity = 1024 * 1024 * 20;  // 20MB
 - (void)computeWebViewUserAgent {
     static dispatch_once_t onceToken;
     self.webView = [[WKWebView alloc] initWithFrame:CGRectZero];
+    [self.webView loadHTMLString:@"<html></html>" baseURL:nil];
+    __weak typeof(self) weakSelf = self;
     dispatch_once_on_main_thread(&onceToken, ^{
-        [self.webView loadHTMLString:@"<html></html>" baseURL:nil];
-        [self.webView evaluateJavaScript:@"navigator.userAgent" completionHandler:^(id __nullable userAgent, NSError * __nullable error) {
-            self.webViewUserAgent = userAgent;
-            self.webView = nil;
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        [strongSelf.webView evaluateJavaScript:@"navigator.userAgent" completionHandler:^(id __nullable userAgent, NSError * __nullable error) {
+            strongSelf.webViewUserAgent = userAgent;
+            strongSelf.webView = nil;
         }];
     });
 }
