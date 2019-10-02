cde8f05586b6c504de7ac9d781900ef7a90f75df
Arnold Goldberg <agoldberg@salesforce.com>
2019-10-02 11:05:01 -0500

    Cherry Pick acb72d8fba2ffcb269f7addd3dd3b5e76fc700c4

--- libs/SmartStore/SmartStore/Classes/SFSmartStore.m
+++ libs/SmartStore/SmartStore/Classes/SFSmartStore.m
@@ -67,6 +67,7 @@ NSString * const kDefaultSmartStoreName   = @"defaultStore";
 NSString * const kSFAppFeatureSmartStoreUser   = @"US";
 NSString * const kSFAppFeatureSmartStoreGlobal   = @"GS";
 NSString * const kSFSmartStoreJSONParseErrorNotification = @"SFSmartStoreJSONParseErrorNotification";
+NSString * const kSFSmartStoreJSONSerializationErrorNotification = @"SFSmartStoreJSONSerializationErrorNotification";
 
 // NSError constants  (TODO: We should move this stuff into a framework where errors can be configurable
 // in a plist, once we start delivering a bundle.
@@ -855,10 +856,20 @@ NSString *const EXPLAIN_ROWS = @"rows";
     log(@"1/4 Starting to write to tmp file");
     [outputStream open];
     NSError *error = nil;
-    BOOL success = [NSJSONSerialization writeJSONObject:soupEntry
-                                               toStream:outputStream
-                                                options:0
-                                                  error:&error];
+
+    // NSJSONSerialization:writeJSONObject returns the number of bytes written
+    // So NSJSONSerialization:writeJSONObject can return a value > 0 while there is an error
+    [NSJSONSerialization writeJSONObject:soupEntry
+                                toStream:outputStream
+                                 options:0
+                                   error:&error];
+    if (error) {
+        [SFSDKSmartStoreLogger e:[self class] format:@"Error serializing JSON in SmartStore in %@", NSStringFromSelector(_cmd)];
+        [SFSmartStore buildEventOnJsonSerializationErrorForUser:self.user fromMethod:NSStringFromSelector(_cmd) error:error];
+    }
+    
+    BOOL success = !error;
+
     [outputStream close];
     log(@"2/4 Done writing to tmp file");
 
@@ -902,18 +913,28 @@ NSString *const EXPLAIN_ROWS = @"rows";
     return [SFJsonUtils objectFromJSONString:[self loadExternalSoupEntryAsString:soupEntryId soupTableName:soupTableName]];
 }
 
-+ (void)buildEventOnJsonErrorForUser:(SFUserAccount *)user {
++ (void)buildEventOnJsonParseErrorForUser:(SFUserAccount *)user fromMethod:(NSString*)fromMethod {
     NSMutableDictionary *attributes = [[NSMutableDictionary alloc] init];
     attributes[@"errorCode"] = [NSNumber numberWithInteger:SFJsonUtils.lastError.code];
     attributes[@"errorMessage"] = SFJsonUtils.lastError.localizedDescription;
+    attributes[@"fromMethod"] = fromMethod;
     [SFSDKEventBuilderHelper createAndStoreEvent:@"SmartStoreJSONParseError" userAccount:user className:NSStringFromClass([self class]) attributes:attributes];
     [[NSNotificationCenter defaultCenter] postNotificationName:kSFSmartStoreJSONParseErrorNotification object:self];
+}
+
++ (void)buildEventOnJsonSerializationErrorForUser:(SFUserAccount *)user fromMethod:(NSString*)fromMethod error:(NSError*)error {
+    NSMutableDictionary *attributes = [[NSMutableDictionary alloc] init];
+    attributes[@"errorCode"] = [NSNumber numberWithInteger:error.code];
+    attributes[@"errorMessage"] = error.localizedDescription;
+    attributes[@"fromMethod"] = fromMethod;
+    [SFSDKEventBuilderHelper createAndStoreEvent:@"SmartStoreJSONSerializationError" userAccount:user className:NSStringFromClass([self class]) attributes:attributes];
+    [[NSNotificationCenter defaultCenter] postNotificationName:kSFSmartStoreJSONSerializationErrorNotification object:self];
 }
 
 - (BOOL)checkRawJson:(NSString*)rawJson fromMethod:(NSString*)fromMethod {
     if (_jsonSerializationCheckEnabled && [SFJsonUtils objectFromJSONString:rawJson] == nil) {
         [SFSDKSmartStoreLogger e:[self class] format:@"Error parsing JSON in SmartStore in %@", fromMethod];
-        [SFSmartStore buildEventOnJsonErrorForUser:self.user];
+        [SFSmartStore buildEventOnJsonParseErrorForUser:self.user fromMethod:fromMethod];
         return NO;
     } else {
         return YES;
