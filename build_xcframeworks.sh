b0fc2d464637e2b452b60c9bff75569946bd0ee4
Wolfgang Mathurin <wmathurin@salesforce.com>
2023-07-06 12:35:56 -0600

    Script to build xcframeworks for all the libraries

---
+++ build_xcframeworks.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#set -x
+
+function header() {
+    local SPACER="---------------------------------------------------------------------------"
+    echo -e "\n\n$SPACER\n    $1\n$SPACER\n"
+}
+
+
+
+for lib in 'SalesforceSDKCommon' 'SalesforceAnalytics' 'SalesforceSDKCore' 'SmartStore' 'MobileSync'
+do
+    header "Building iOS archive for $lib"
+    xcodebuild archive \
+        -workspace SalesforceMobileSDK.xcworkspace \
+        -scheme $lib \
+        -destination "generic/platform=iOS" \
+        -archivePath ./archives/$lib-iOS \
+        SKIP_INSTALL=NO \
+        BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+    header "Building iOS simulator archive for $lib"
+    xcodebuild archive \
+        -workspace SalesforceMobileSDK.xcworkspace \
+        -scheme $lib \
+        -destination "generic/platform=iOS Simulator" \
+        -archivePath ./archives/$lib-Sim \
+        SKIP_INSTALL=NO \
+        BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+    header "Building xcframework for $lib"
+    xcodebuild -create-xcframework \
+        -framework ./archives/$lib-iOS.xcarchive/Products/Library/Frameworks/$lib.framework \
+        -framework ./archives/$lib-Sim.xcarchive/Products/Library/Frameworks/$lib.framework \
+        -output ./archives/$lib.xcframework
+
+    header "Zipping xcframework for $lib"
+    cd archives
+    zip $lib.xcframework.zip $lib.xcframework -r
+    cd ..
+
+    header "Cleaning up intermediate files for $lib"
+    rm -rf ./archives/$lib-iOS.xcarchive
+    rm -rf ./archives/$lib-Sim.xcarchive
+    rm -rf ./archives/$lib.xcframework
+
+done
